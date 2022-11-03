9e4c7b9998360355387a47b5b5a82d186a0d3aab
Sebastián López Osorno <100486485+SebastianLopezOsorno-SENA@users.noreply.github.com>
2022-11-02 22:45:13 -0500

    Add files via upload

---
+++ animacion ascii/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using static System.Net.WebRequestMethods;
+
+
+class Init
+{
+    static void Main()
+    {
+        string name;
+        int frames;
+
+        Console.WriteLine("Ingrese el nombre del archivo: ");
+        name =Console.ReadLine();
+
+        Console.WriteLine("Ingrese los frames de la imagen: ");
+        frames = int.Parse(Console.ReadLine());
+
+        Console.Clear();
+
+        string path = Path.GetFullPath(@"..\..\Source\"+name+".txt");
+        while (true) 
+        { 
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        if (line=="break")
+                        {
+                            Thread.Sleep(frames);
+                            Console.Clear();
+                            continue;
+                        }
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
